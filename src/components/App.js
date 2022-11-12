8a117442626965e94239b4235f39eac9bc711587
Jordan <jordan.su@protonmail.com>
2022-11-11 20:38:18 -0500

    CU-3a1v49t[complete] - Implement Redux. Basic implementation using redux toolkit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import Container from './Container';
 import Header from './Header';
 import Footer from './Footer';
+import Counter from '../features/counter/Counter';
+
 function App() {
     return (
         <Container>
             <Header />
+            <Counter />
             <Footer />
         </Container>
     );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import { ColorModeScript, ChakraProvider } from '@chakra-ui/react';
 import theme from './theme';
+
+import App from './components/App';
+// import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <ChakraProvider theme={theme}>
-            <App />
-        </ChakraProvider>
+        <Provider store={store}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <ChakraProvider theme={theme}>
+                <App />
+            </ChakraProvider>
+        </Provider>
     </React.StrictMode>
 );
