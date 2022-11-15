2d08edcca8aa55d249645f4e877bf5c34eb625d8
Jordan Holt <66709081+jordanholtdev@users.noreply.github.com>
2022-11-15 02:42:38 +0000

    CU-3ak37ck[in progress] - Fix sticky footer (#11)

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,7 @@ import { Flex } from '@chakra-ui/react';
 export const Container = (props) => {
     return (
         <Flex
+            minH='100vh'
             direction='column'
             alignItems='center'
             justifyContent='flex-start'
