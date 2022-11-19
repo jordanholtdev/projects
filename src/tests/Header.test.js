b866a0482d7cdc721a69221c98111f5b1d53830c
Jordan <jordan.su@protonmail.com>
2022-11-18 23:29:21 -0500

    update testing-library/user-event package

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,17 +1,31 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../components/Header';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders Header component', () => {
-    render(<Header />);
+    render(<Header />, { wrapper: BrowserRouter });
     const headerElement = screen.getByRole('heading', { level: 1 });
     expect(headerElement).toBeInTheDocument();
     expect(headerElement).toBeVisible();
 });
 
 test('renders Github link', () => {
-    render(<Header />);
+    render(<Header />, { wrapper: BrowserRouter });
     const githubLinkElement = screen.getByLabelText(
         'Navigate to Jordan Holt Github Repository'
     );
     expect(githubLinkElement).toBeInTheDocument();
+});
+
+test('navigating', async () => {
+    render(<Header />, { wrapper: BrowserRouter });
+    const user = userEvent.setup();
+
+    // verify page content for default route
+    expect(screen.getByText(/Projects/i)).toBeInTheDocument();
+
+    // verify page content for expected route after navigating
+    await user.click(screen.getByText(/Projects/i));
+    expect(screen.getByText(/Projects/i)).toBeInTheDocument();
 });
