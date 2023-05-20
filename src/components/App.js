70a417fa667a4b77ccf0c3d3ca236cf1a200163d
Jordan <jordan.su@protonmail.com>
2023-05-19 22:56:25 -0400

    CU-860qxmm8p - update the app loading component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,77 @@
 import Container from './Container';
 import Header from './Header';
+import React, { useEffect } from 'react';
 import Footer from './Footer';
 import ProjectsList from '../features/projects/ProjectsList';
 import ProjectTags from '../features/projects/ProjectTags';
-import { useDispatch } from 'react-redux';
-import { fetchProjects, fetchTags } from '../features/projects/projectsSlice';
-import { Flex, Box } from '@chakra-ui/react';
+import { useDispatch, useSelector } from 'react-redux';
+import AppLoad from './AppLoad';
+import {
+    fetchProjects,
+    fetchTags,
+    getProjectsLoadingStatus,
+} from '../features/projects/projectsSlice';
 
-function App() {
+import { Stack, Box } from '@chakra-ui/react';
+
+const App = () => {
     const dispatch = useDispatch();
-    dispatch(fetchProjects());
-    dispatch(fetchTags());
+    const projectsLoadingStatus = useSelector(getProjectsLoadingStatus);
+
+    // Fetch projects and tags if they are not already loaded
+    useEffect(() => {
+        dispatch(fetchProjects());
+        dispatch(fetchTags());
+    }, [dispatch]);
+
+    let content;
+
+    // Render loading state if projects are still loading
+    if (projectsLoadingStatus === 'pending') {
+        return (
+            <Container>
+                <Header />
+                <Box
+                    maxW='1280px'
+                    px={10}
+                    pt={10}
+                    as='section'
+                    justifyContent='center'
+                >
+                    <AppLoad />
+                </Box>
+                <Footer />
+            </Container>
+        );
+    } else if (projectsLoadingStatus === 'succeeded') {
+        // Render the app if projects are loaded successfully
+        content = (
+            <Box maxW='1280px' px={10} pt={10} as='section'>
+                <Stack
+                    spacing={10}
+                    align='flex-start'
+                    direction={['column', 'row']}
+                    minW='full'
+                >
+                    <Box pr={[5, 10, 20]}>
+                        <ProjectTags />
+                    </Box>
+                    <Box display='block' minW='full'>
+                        <ProjectsList />
+                    </Box>
+                </Stack>
+            </Box>
+        );
+    } else if (projectsLoadingStatus === 'failed') {
+        content = <p>failed...</p>;
+    }
     return (
         <Container>
             <Header />
-            <Box maxW='1280px' px={10} pt={10} as='section'>
-                <Flex flexDirection={['column', 'row']}>
-                    <Box mr={[5, 10, 20]}>
-                        <ProjectTags />
-                    </Box>
-                    <ProjectsList />
-                </Flex>
-            </Box>
+            {content}
             <Footer />
         </Container>
     );
-}
+};
 
 export default App;
